d36bbc7273127d72989ffca09e206ec9db8f5142
wezer-pixel <jerryjuma104@gmail.com>
2024-02-03 18:01:23 +0300

    utf-8 validator

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -1,29 +1,41 @@
-#!/usr/bin/env python3
-
-"""Function validates utf-8 by running utf-8 checks on input"""
 def validUTF8(data):
-    if not data:
-        return False
+    """Checks if a list of integers are valid UTF-8 codepoints.
+    See <https://datatracker.ietf.org/doc/html/rfc3629#page-4>
+    """
+    if not isinstance(data, list):
+        raise TypeError("Input must be a list of integers.")
+    
+    skip = 0
+    n = len(data)
+    span = 0
+    
+    for i in range(n):
+        if skip > 0:
+            skip -= 1
+            continue
 
-    num_bytes = 0
-
-    for num in data:
-        bin_num = format(num, '08b')
-
-        if num_bytes == 0:
-            if bin_num.startswith("0"):
-                continue
-            elif bin_num.startswith("110"):
-                num_bytes = 1
-            elif bin_num.startswith("1110"):
-                num_bytes = 2
-            elif bin_num.startswith("11110"):
-                num_bytes = 3
-            else:
-                return False
+        if type(data[i]) != int or data[i] < 0 or data[i] > 0x10FFFF:
+            return False
+        elif data[i] <= 0x7F:
+            skip = 0
+        elif data[i] & 0b11111000 == 0b11110000:
+            span = 4
+        elif data[i] & 0b11110000 == 0b11100000:
+            span = 3
+        elif data[i] & 0b11100000 == 0b11000000:
+            span = 2
         else:
-            if not bin_num.startswith("10"):
+            return False
+        
+        if n - i >= span:
+            next_body = list(map(
+                lambda x: x & 0b11000000 == 0b10000000,
+                data[i + 1: i + span],
+            ))
+            if not all(next_body):
                 return False
-            num_bytes -= 1
-
-    return num_bytes == 0
+            skip = span - 1
+        else:
+            return False
+    
+    return True
