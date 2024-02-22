7f53b3ae309159006467d6b5c3f21e940403f58d
wezer-pixel <jerryjuma104@gmail.com>
2024-02-22 17:31:33 +0300

    rotate matrix clockwise

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -1,9 +1,25 @@
+#!/usr/bin/python3
+"""
+Rotate 2D Matrix
+"""
+
 def rotate_2d_matrix(matrix):
+    """rotate two dimension matrix 90 degrees clockwise
+    Args:
+        matrix (list[[list]]): a matrix
+    """
     n = len(matrix)
     for i in range(n // 2):
-        for j in range(i, n - i - 1):
-            temp = matrix[i][j]
-            matrix[i][j] = matrix[n - 1 - j][i]
-            matrix[n - 1 - j][i] = matrix[n - 1 - i][n - 1 - j]
-            matrix[n - 1 - i][n - 1 - j] = matrix[j][n - 1 - i]
-            matrix[j][n - 1 - i] = temp
+        y = n - i - 1
+        for j in range(i, y):
+            x = n - 1 - j
+            # current number
+            tmp = matrix[i][j]
+            # change top for left
+            matrix[i][j] = matrix[x][i]
+            # change left for bottom
+            matrix[x][i] = matrix[y][x]
+            # change bottom for right
+            matrix[y][x] = matrix[j][y]
+            # change right for top
+            matrix[j][y] = tmp
