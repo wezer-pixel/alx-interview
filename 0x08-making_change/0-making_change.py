923b32616d61457ff490ac8ea311ed4ff6575139
wezer-pixel <jerryjuma104@gmail.com>
2024-03-03 20:38:14 +0300

    Making change

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+
+""" Contains makeChange function"""
+
+
+def makeChange(coins, total):
+    """
+    Returns the fewest number of coins needed to meet the total.
+
+    Args:
+        coins (list): A list of coin denominations.
+        total (int): The target total amount.
+
+    Returns:
+        int: The fewest number of coins needed to meet the total.
+
+    Notes:
+        - If the total is 0 or less, returns 0.
+        - If the total cannot be met by any number of coins you have, returns -1.
+    """
+    if not coins or coins is None:
+        return -1
+    if total <= 0:
+        return 0
+    change = 0
+    coins = sorted(coins)[::-1]
+    for coin in coins:
+        while coin <= total:
+            total -= coin
+            change += 1
+        if total == 0:
+            return change
+    return -1
